bb93b51edeb4ba52ef6e9f5ba43ff1d1a9b1ec49
Vuk Marković <vuk.markovic@gsm-nis.edu.rs>
2022-01-27 17:36:58 +0100

    signalr adding hub name

--- Dualscape.RTC/Hubs/GameStateHub.cs
+++ Dualscape.RTC/Hubs/GameStateHub.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
 using Microsoft.AspNetCore.SignalR;
 
 
 namespace Dualscape.RTC.Hubs
 {
+    [HubName("GameStateHub")]
     public class GameStateHub : Hub
     {
         public async Task UpdateGameState(string delta)
